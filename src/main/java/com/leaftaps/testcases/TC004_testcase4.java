167d78e51535a1d01b9dfea3a66125e1c054c8d6
POOVARASAN MAHENDIRAN -X (pmahendi - HCL TECHNOLOGIES LIMITED at Cisco) <pmahendi@cisco.com>
2023-09-02 21:37:55 +0530

    pushing tc4 in feature 112 branch

---
+++ src/main/java/com/leaftaps/testcases/TC004_testcase4.java
@@ -0,0 +1,28 @@
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectHooks;
+import com.leaftaps.pages.LoginPage;
+
+public class TC002_JustLogin extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Navigation";
+		testDescription ="Navigate to Leaftaps";
+		authors="poo";
+		category ="regression";
+		excelFileName="Navigation";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+
+	}
+
+}
